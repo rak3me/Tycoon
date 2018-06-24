e5941ec4571691eb07024a4e4bffbb8e6cfab152
Peter Cho <chopeter3@gmail.com>
2018-06-24 16:44:36 -0500

    Camera change finally fucking worked woohoo

--- Unity Projects/PotionSeller/Assets/Scripts/PlayerController.cs
+++ Unity Projects/PotionSeller/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour {
 
     public LayerMask movementMask;
 
+    private RaycastHit hit;
+
     Camera cam;
     PlayerMotor motor;
+
 
 	// Use this for initialization
 	void Start () {
@@ -21,7 +24,6 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
