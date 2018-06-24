98bb14a5e363efac0d08e0d1c6d86b83fba78f88
Peter Cho <chopeter3@gmail.com>
2018-06-24 00:46:24 -0500

    Player Movement and Camera Changes

---
+++ Unity Projects/PotionSeller/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject newCamera;
+    public int speed;
+
+    Camera cam;
+
+    private bool triggerEntered;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerEntered = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (triggerEntered)
+        {
+            cam.transform.position = 
+                Vector3.Lerp(cam.transform.position, newCamera.transform.position, speed * Time.deltaTime);
+        }
+	}
+}
---
+++ Unity Projects/PotionSeller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+	}
+}
