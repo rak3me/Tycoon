237ee9545aeb4294e02781e9664385cc0c847e63
Ryan Kann <rak3me@virginia.edu>
2018-06-24 17:00:28 -0400

    Editor Create Item almost implemented

---
+++ Unity Projects/PotionSeller/Assets/Scripts/Editor/ItemWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ItemWindow : EditorWindow {
+
+    string itemName = "DefaultName";
+    string itemDescription = "";
+    Sprite itemIcon;
+    GameObject itemModel;
+    int itemCost = 1;
+
+
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    [MenuItem("GameObject/Create Item")]
+    public static void Init () {
+        EditorWindow.GetWindow<ItemWindow>("Create Item");
+    }
+
+
+    private void OnGUI () {
+        GUILayout.Label ("Item Properties", EditorStyles.boldLabel);
+        itemName = EditorGUILayout.TextField ("Item Name", itemName);
+        itemDescription = EditorGUILayout.TextField("Description", itemDescription);
+        itemCost = EditorGUILayout.IntSlider("Item Cost", itemCost, 0, 500);
+        itemIcon = (Sprite)EditorGUILayout.ObjectField("Item Icon", itemIcon, typeof(Sprite), false);
+        itemModel = (GameObject)EditorGUILayout.ObjectField("Item Model", itemModel, typeof(GameObject), false);
+
+        if (GUILayout.Button("Create")) {
+            CreateItem();
+        }
+    }
+
+    private void CreateItem () {
+        //new Item(itemName, Random.Range(0, int.MaxValue), itemDescription, itemIcon, itemModel)
+        GameObject itemGO = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //Item item = itemGO.AddComponent<Item>();
+        //item = new Item(itemName, Random.Range(0, int.MaxValue), itemDescription, itemIcon, itemModel);
+
+        Object temp = PrefabUtility.CreateEmptyPrefab("Assets/Resources/Prefabs/Items/" + itemName + ".prefab");
+        PrefabUtility.ReplacePrefab(itemGO, temp);
+        DestroyImmediate(itemGO);
+        //AssetDatabase.CreateAsset(new Object(), "Assets/Resources/Prefabs/Items/" + itemName + ".asset");
+    }
+}
---
+++ Unity Projects/PotionSeller/Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;                                     //itemName of the item
+    public int itemID;                                          //itemID of the item
+    public string itemDesc;                                     //itemDesc of the item
+    public Sprite itemIcon;                                     //itemIcon of the item
+    public GameObject itemModel;                                //itemModel of the item
+    public int itemCost = 1;                                   //itemValue is at start 1
+    
+    public Item(){}
+
+    public Item(string name, int id, string desc, Sprite icon, GameObject model)                 //function to create a instance of the Item
+    {
+        itemName = name;
+        itemID = id;
+        itemDesc = desc;
+        itemIcon = icon;
+        itemModel = model;
+    }
+
+    public Item getCopy()
+    {
+        return (Item)this.MemberwiseClone();        
+    }   
+    
+    
+}
+
+
